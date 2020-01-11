49c14cf91cd0a0339e1a38fb4365001889c373ca
Sebastian <sebthomle@gmail.com>
2020-01-11 01:52:22 +0100

    Now will not run bot when testing

--- bot.js
+++ bot.js
@@ -11,42 +11,46 @@ logger.add(new logger.transports.Console, {
 });
 
 logger.level = 'info';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+if(process.env.NODE_ENV != "test"){
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    message = message.toLowerCase()
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+    // Initialize Discord Bot
+    var bot = new Discord.Client({
+        token: auth.token,
+        autorun: true
+    });
 
-        args = args.splice(1);
-        switch (cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong! '
-                });
-                break;
+    bot.on('ready', function (evt) {
+        logger.info('Connected');
+        logger.info('Logged in as: ');
+        logger.info(bot.username + ' - (' + bot.id + ')');
+    });
 
-            case "order":
-                Order(user, args)
-                break;
+    bot.on('message', function (user, userID, channelID, message, evt) {
+        message = message.toLowerCase()
+        // Our bot needs to know if it will execute a command
+        // It will listen for messages that will start with `!`
+        if (message.substring(0, 1) == '!') {
+            var args = message.substring(1).split(' ');
+            var cmd = args[0];
+
+            args = args.splice(1);
+            switch (cmd) {
+                // !ping
+                case 'ping':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Pong! '
+                    });
+                    break;
+
+                case "order":
+                    Order(user, args)
+                    break;
+            }
         }
-    }
-});
+    });
+}
 
 let sizeConv = {
     "s": "small",
